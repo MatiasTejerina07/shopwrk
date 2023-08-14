2d72e1af7d9ee9f9e34f8945eabc6f3ebc0a9127
Matias Tejerina <127890622+MatiasTejerina07@users.noreply.github.com>
2023-08-14 12:56:57 -0300

    Card product (#6)
    * vistas-card
    
    * card ok

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,22 +1,51 @@
 import { IProduct } from "@/interfaces";
-import { Card, CardActionArea, CardMedia, Grid } from "@mui/material";
-import React, { FC } from "react";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
+import Image from "next/image";
+import React, { FC, useMemo, useState } from "react";
 
 interface Props {
   product: IProduct;
 }
 export const ProductCard: FC<Props> = ({ product }) => {
+  const [isHovered, setisHovered] = useState(false);
+
+  const productImage = useMemo(() => {
+    return isHovered
+      ? `products/${product.images[1]}`
+      : `products/${product.images[0]}`;
+  }, [isHovered, product.images]);
+
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setisHovered(true)}
+      onMouseLeave={() => setisHovered(false)}
+    >
       <Card>
         <CardActionArea>
           <CardMedia
+            className="fadeIn"
             component="img"
-            image={`products/${product.images[0]}`}
+            image={productImage}
             alt={product.title}
+            onLoad={() => <CircularProgress />}
           />
         </CardActionArea>
       </Card>
+      <Box className="fadeIn" sx={{ mt: 1 }}>
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>${product.price}</Typography>
+      </Box>
     </Grid>
   );
 };
