31bfe8fe012a6aa950f12c53364ef5b60fe81ced
Mat <matiastejerina94@gmail.com>
2023-08-14 10:09:50 -0300

    falta colocar una imagen

---
+++ pages/404.tsx
@@ -0,0 +1,29 @@
+import { ShopLayout } from "@/components/layouts";
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const CustomPageError = () => {
+  return (
+    <ShopLayout
+      title={"Page not found"}
+      pageDescription={"No hay nada que mostrar aquí"}
+    >
+      <Box
+        sx={{ flexDirection: { xs: "column", sm: "row" } }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="calc(100vh - 200px)"
+      >
+        <Typography variant="h1" component="h1" fontSize={80} fontWeight={200}>
+          404 |
+        </Typography>
+        <Typography marginLeft={2}>
+          No encontramos ninguna página aquí
+        </Typography>
+      </Box>
+    </ShopLayout>
+  );
+};
+
+export default CustomPageError;
