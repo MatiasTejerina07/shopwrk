920060b3e45f5bdc79a26559d8c12037688beb4f
Matias Tejerina <127890622+MatiasTejerina07@users.noreply.github.com>
2023-08-14 10:22:04 -0300

    hardcorder-vista-home (#4)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,44 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import { ShopLayout } from "@/components/layouts";
-import { Typography } from "@mui/material";
-
-const inter = Inter({ subsets: ["latin"] });
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { initialData } from "@/database/products";
 
 export default function Home() {
   return (
-    <ShopLayout
-      title={"Shop | WRK"}
-      pageDescription={"Encuentra los mejores productos de ropa e idumentaria"}
-    >
-      <Typography variant="h1" component="h1">
-        Tienda
-      </Typography>
-      <Typography variant="h2" sx={{ mb: 1 }} component="h1">
-        Todos los productos
-      </Typography>
-    </ShopLayout>
+    <>
+      <ShopLayout
+        title={"Shop | WRK"}
+        pageDescription={
+          "Encuentra los mejores productos de ropa e idumentaria"
+        }
+      >
+        <Typography variant="h1" component="h1">
+          Tienda
+        </Typography>
+        <Typography variant="h2" sx={{ mb: 1 }} component="h1">
+          Todos los productos
+        </Typography>
+      </ShopLayout>
+      <Grid container spacing={4}>
+        {initialData.products.map((product) => (
+          <Grid item xs={6} sm={4} key={product.slug}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={`products/${product.images[0]}`}
+                  alt={product.title}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
